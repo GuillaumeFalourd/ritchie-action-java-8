823c6adb2e596c3e355f420a021e3d25c96d9a01
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-01 18:31:59 -0300

    update entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -l
 
-# Import formula repository command
-sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1
+if [ -z "$CREDENTIAL_GITHUB_TOKEN" ]
+then
+      echo "PUBLIC 🔓 repository workflow detected"
+      # Import public formula repository command
+      sudo rit add repo --provider=Github --name=public-repo --repoUrl=$1 --priority=1
+else
+      echo "PRIVATE 🔐 repository workflow detected"
+      # Add Github user credentials
+      sudo rit set credential --provider=github --fields=username,token --values="$CREDENTIAL_GITHUB_USERNAME","$CREDENTIAL_GITHUB_TOKEN"
+      # Import private formula repository command
+      sudo rit add repo --provider=Github --name=private-repo --repoUrl=$1 --priority=1 --token=$CREDENTIAL_GITHUB_TOKEN
+fi
 
 # Execute formula command
 sudo $2
